3743a34a3ad7e355ec5696073e195761c844835d
ebmangabeira <ebmangabeira@gmail.com>
2025-06-24 21:59:17 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: eStore
-* Template URL: https://bootstrapmade.com/estore-bootstrap-ecommerce-template/
-* Updated: Mar 01 2025 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
