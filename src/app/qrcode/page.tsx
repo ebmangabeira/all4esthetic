0672442e787b958253c4181c6a4b174e8d568f34
mariorui97 <marinhocosta@live.com.pt>
2025-10-20 22:35:07 +0100

    add query parameter

--- src/app/qrcode/page.tsx
+++ src/app/qrcode/page.tsx
@@ -2,5 +2,13 @@
 import { redirect } from 'next/navigation';
 
 export default function Page({ searchParams }) {
-  redirect(`http://localhost:7777/index.php?module=IFE_Report&action=index&record=e5066d95-55fa-08fb-9ffb-61fe4eb7a95d&name=Agendamentos&type=tasks&qrcode=c`);
+  const c = searchParams?.c;
+
+  if (!c) {
+    return <p>Missing query parameter "c".</p>; // simple fallback
+  }
+
+  redirect(
+    `http://localhost:7777/index.php?module=IFE_Report&action=index&record=e5066d95-55fa-08fb-9ffb-61fe4eb7a95d&name=Agendamentos&type=tasks&qrcode=${encodeURIComponent(c)}`
+  );
 }
